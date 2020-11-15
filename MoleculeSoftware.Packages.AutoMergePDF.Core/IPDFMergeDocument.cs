8497e949da1638e5335f4339710a7b3afc277214
Joshua Santiago <joshman1019@gmail.com>
2020-11-15 00:25:11 -0500

    Add .NET Core 3.1 project and duplicate all components into it

---
+++ MoleculeSoftware.Packages.AutoMergePDF.Core/IPDFMergeDocument.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoMergePDF
+{
+    public interface IPDFMergeDocument
+    {
+         string IdentificationMarker { get; set; }
+         string PDF1Path { get; set; }
+         string PDF2Path { get; set; }
+        string OutputPath { get; set; }
+    }
+}
