9a4cf28661dabcbb6df503a07eee1b13f8762ea7
joshman1019 <36560776+joshman1019@users.noreply.github.com>
2021-03-06 13:08:33 -0500

    Constructed MultipleMergeDocument object

--- MoleculeSoftware.Packages.AutoMergePDF.Core/MergeDocumentGenerator.cs
+++ MoleculeSoftware.Packages.AutoMergePDF.Core/MergeDocumentGenerator.cs
@@ -12,27 +12,30 @@ namespace AutoMergePDF
             m_Document = document;
         }
 
+        /// <summary>
+        /// Generates the merged documents
+        /// </summary>
         public void CreateMergedDocument()
         {
             // Guard clauses
-            if (string.IsNullOrEmpty(m_Document.OutputPath))
+            if(string.IsNullOrEmpty(m_Document.OutputPath))
             {
-                throw new System.Exception("Exception 1: Output path was null or empty");
+                throw new System.Exception("Exception 1: Output path was null or empty"); 
             }
 
-            if (string.IsNullOrEmpty(m_Document.PDF1Path))
+            if(string.IsNullOrEmpty(m_Document.PDF1Path))
             {
-                throw new System.Exception("Exception 2: Document 1 input path was null or empty");
+                throw new System.Exception("Exception 2: Document 1 input path was null or empty"); 
             }
 
-            if (string.IsNullOrEmpty(m_Document.PDF2Path))
+            if(string.IsNullOrEmpty(m_Document.PDF2Path))
             {
-                throw new System.Exception("Exception 3: Document 2 input path was null or empty");
+                throw new System.Exception("Exception 3: Document 2 input path was null or empty"); 
             }
 
-            if (!System.IO.File.Exists(m_Document.PDF1Path))
+            if(!System.IO.File.Exists(m_Document.PDF1Path))
             {
-                throw new System.Exception("Exception 4: Input file number 1 does not exist at the location provided");
+                throw new System.Exception("Exception 4: Input file number 1 does not exist at the location provided"); 
             }
 
             if (!System.IO.File.Exists(m_Document.PDF2Path))
---
+++ MoleculeSoftware.Packages.AutoMergePDF/IPDFMultipleMergeDocuments.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AutoMergePDF
+{
+    public interface IPDFMultipleMergeDocuments
+    {
+        string IdentificationMarker { get; set; }
+        List<string> InputDocumentPaths { get; set; }
+        string OutputDocumentPath { get; set; }
+    }
+}
